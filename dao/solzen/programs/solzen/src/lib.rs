09869564f72038c928ce062985977770fa27a56b
Fabio Issamu Oshiro <fabio.oshiro@gmail.com>
2022-07-04 00:13:43 -0300

    inicio de validacao manual

--- dao/solzen/programs/solzen/src/lib.rs
+++ dao/solzen/programs/solzen/src/lib.rs
@@ -1,15 +1,52 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("2QB8wEBJ8jjMQuZPvj3jaZP7JJb5j21u4xbxTnwsZRfv");
 
 #[program]
 pub mod solzen {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn validate_human(ctx: Context<ValidateHuman>, child: Pubkey) -> Result<()> {
+        let validation: &mut Account<Validation> = &mut ctx.accounts.validation;
+        let parent: &Signer = &ctx.accounts.parent;
+        validation.parent = *parent.key;
+        validation.child = child;
+        let clock: Clock = Clock::get().unwrap();
+        validation.timestamp = clock.unix_timestamp;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(child: Pubkey)]
+pub struct ValidateHuman<'info> {
+    #[account(init, payer = parent, space = Validation::LEN,
+        seeds = [b"child", child.as_ref()], bump)]
+    pub validation: Account<'info, Validation>,
+
+    #[account(mut)]
+    pub parent: Signer<'info>,
+
+    #[account(address = system_program::ID)]
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Validation {
+    pub parent: Pubkey,
+    pub child: Pubkey,
+    pub timestamp: i64,
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+
+impl Validation {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH // parent.
+        + PUBLIC_KEY_LENGTH // child.
+        + TIMESTAMP_LENGTH; // timestamp.
+        
+}
--- dao/solzen/tests/solzen.ts
+++ dao/solzen/tests/solzen.ts
@@ -1,16 +1,89 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey";
+import { expect } from "chai";
 import { Solzen } from "../target/types/solzen";
+const util = require('util')
 
 describe("solzen", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.AnchorProvider.env()
+
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Solzen as Program<Solzen>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
+  it("should validate an user as human", async () => {
+    const child = anchor.web3.Keypair.generate()
+    const [userAccount, _bump] = findProgramAddressSync([
+      anchor.utils.bytes.utf8.encode('child'),
+      child.publicKey.toBuffer()
+    ], program.programId);
+    const tx = await program.methods
+      .validateHuman(child.publicKey)
+      .accounts({
+        validation: userAccount,
+      })
+      .rpc()
+
+
     console.log("Your transaction signature", tx);
+
+    const validationAccount = await program.account.validation.all();
+    expect(validationAccount.length).eq(1)
+    const valAcc = await program.account.validation.fetch(userAccount)
+    expect(valAcc.parent.toBase58()).to.eq(provider.wallet.publicKey.toBase58())
+    expect(valAcc.child.toBase58()).to.eq(child.publicKey.toBase58())
   });
+
+  it("should validate the parent signature", async () => {
+    const wrongSigner = anchor.web3.Keypair.generate()
+    const child = anchor.web3.Keypair.generate()
+    const [userAccount, _bump] = findProgramAddressSync([
+      anchor.utils.bytes.utf8.encode('child'),
+      child.publicKey.toBuffer()
+    ], program.programId)
+
+    const error = await program.methods
+      .validateHuman(child.publicKey)
+      .accounts({
+        validation: userAccount,
+        parent: wrongSigner.publicKey
+      })
+      .rpc()
+      .catch(e => e)
+
+    expect(error.message).to.equal('Signature verification failed')
+  });
+
+  it("should validate only 1x", async () => {
+    const child = anchor.web3.Keypair.generate()
+    const [userAccount, _bump] = findProgramAddressSync([
+      anchor.utils.bytes.utf8.encode('child'),
+      child.publicKey.toBuffer()
+    ], program.programId);
+
+    const tx = await program.methods
+      .validateHuman(child.publicKey)
+      .accounts({
+        validation: userAccount,
+      })
+      .rpc()
+
+    await new Promise((resolve) => setTimeout(() => resolve(1), 1000))
+
+    console.log("Your transaction signature", tx);
+    const tx2 = await program.methods
+      .validateHuman(child.publicKey)
+      .accounts({
+        validation: userAccount,
+      })
+      .rpc()
+
+    const validationAccount = await program.account.validation.all();
+    expect(validationAccount.length).eq(1)
+    const valAcc = await program.account.validation.fetch(userAccount)
+    expect(valAcc.parent.toBase58()).to.eq(provider.wallet.publicKey.toBase58())
+    expect(valAcc.child.toBase58()).to.eq(child.publicKey.toBase58())
+  })
 });
