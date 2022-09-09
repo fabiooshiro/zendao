7eeb3c001fec065dab3c370c2374ff04a6792be9
Fabio Oshiro <fabio.oshiro@calindra.com.br>
2022-09-09 12:24:35 -0300

    refactor test

--- dao/solzen/programs/solzen/tests/factory.rs
+++ dao/solzen/programs/solzen/tests/factory.rs
@@ -4,18 +4,18 @@ use std::{
 };
 
 use anchor_lang::{
-    prelude::{Account, AccountInfo, Pubkey},
+    prelude::{Account, AccountInfo, Pubkey, Signer, Program},
     solana_program::stake_history::Epoch,
-    AccountSerialize,
+    AccountSerialize, system_program::System,
 };
-use solzen::models::{Zendao, Validation};
+use solzen::models::{Validation, Zendao};
 
 pub struct TestContext {
     pub info_key: Pubkey,
     pub info_owner: Pubkey,
 }
 
-fn create_account_info<'a>(
+pub fn create_account_info<'a>(
     key: &'a Pubkey,
     owner: &'a Pubkey,
     lamports: &'a mut u64,
@@ -64,9 +64,72 @@ pub fn create_validation_account<'a>(
     validation_struct.try_serialize(&mut buffer).unwrap();
     buffer.seek(SeekFrom::Start(0)).unwrap();
     buffer.read_to_end(data).unwrap();
-    let validation_info = create_account_info(&test_ctx.info_key, &test_ctx.info_owner, lamports, data);
+    let validation_info =
+        create_account_info(&test_ctx.info_key, &test_ctx.info_owner, lamports, data);
     let validation = Account::<Validation>::try_from_unchecked(&validation_info).unwrap();
     return validation;
+}
+
+pub fn create_signer_account_info<'a>(
+    key: &'a Pubkey,
+    owner: &'a Pubkey,
+    lamports: &'a mut u64,
+    info_data: &'a mut [u8],
+) -> AccountInfo<'a> {
+    return AccountInfo::new(
+        key,
+        true,
+        false,
+        lamports,
+        info_data,
+        owner,
+        false,
+        Epoch::default(),
+    );
+}
+
+pub fn create_signer<'a>(
+    test_ctx: &'a TestContext,
+    lamports: &'a mut u64,
+    data: &'a mut Vec<u8>,
+) -> Signer {
+    let founder_info =
+        create_signer_account_info(&test_ctx.info_key, &test_ctx.info_owner, lamports, data);
+    let founder = Signer::try_from(&founder_info).unwrap();
+    return founder;
+}
+
+pub fn create_program_account_info<'a>(
+    key: &'a Pubkey,
+    owner: &'a Pubkey,
+    lamports: &'a mut u64,
+    info_data: &'a mut [u8],
+) -> AccountInfo<'a> {
+    return AccountInfo::new(
+        key,
+        true,
+        false,
+        lamports,
+        info_data,
+        owner,
+        true,
+        Epoch::default(),
+    );
+}
+
+pub fn create_system_program<'a>(
+    test_ctx: &'a TestContext,
+    lamports: &'a mut u64,
+    data: &'a mut Vec<u8>,
+) -> Program<System> {
+    let program_info = create_program_account_info(
+        &test_ctx.info_key,
+        &test_ctx.info_owner,
+        lamports,
+        data,
+    );
+    let system_program = Program::try_from(&program_info).unwrap();
+    return system_program;
 }
 
 pub fn create_zendao() -> Zendao {
--- dao/solzen/programs/solzen/tests/integration_test.rs
+++ dao/solzen/programs/solzen/tests/integration_test.rs
@@ -1,9 +1,7 @@
-use std::error::Error;
-use std::io::{Cursor, Read, Seek, SeekFrom, Write};
+use std::io::{Cursor, Read, Seek, SeekFrom};
 use std::{collections::BTreeMap, str::FromStr};
 
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::stake_history::Epoch;
 use sha2::{Digest, Sha256};
 use solzen::entry;
 use solzen::{
@@ -13,61 +11,6 @@ use solzen::{
     InitDAO,
 };
 pub mod factory;
-
-
-fn create_account_info<'a>(
-    key: &'a Pubkey,
-    owner: &'a Pubkey,
-    lamports: &'a mut u64,
-    info_data: &'a mut [u8],
-) -> AccountInfo<'a> {
-    return AccountInfo::new(
-        key,
-        false,
-        false,
-        lamports,
-        info_data,
-        owner,
-        false,
-        Epoch::default(),
-    );
-}
-
-fn create_signer_account_info<'a>(
-    key: &'a Pubkey,
-    owner: &'a Pubkey,
-    lamports: &'a mut u64,
-    info_data: &'a mut [u8],
-) -> AccountInfo<'a> {
-    return AccountInfo::new(
-        key,
-        true,
-        false,
-        lamports,
-        info_data,
-        owner,
-        false,
-        Epoch::default(),
-    );
-}
-
-fn create_program_account_info<'a>(
-    key: &'a Pubkey,
-    owner: &'a Pubkey,
-    lamports: &'a mut u64,
-    info_data: &'a mut [u8],
-) -> AccountInfo<'a> {
-    return AccountInfo::new(
-        key,
-        true,
-        false,
-        lamports,
-        info_data,
-        owner,
-        true,
-        Epoch::default(),
-    );
-}
 
 #[test]
 fn it_should_call_entry_generated_by_anchors_macro() {
@@ -107,11 +50,16 @@ fn it_should_call_entry_generated_by_anchors_macro() {
         };
         let mut buffer = Cursor::new(Vec::new());
         validation_struct.try_serialize(&mut buffer).unwrap();
-        
+
         buffer.seek(SeekFrom::Start(0)).unwrap();
         buffer.read_to_end(&mut validation_buf).unwrap();
-        
-        create_account_info(&info_key, &info_owner, &mut validation_lamports, &mut validation_buf)
+
+        factory::create_account_info(
+            &info_key,
+            &info_owner,
+            &mut validation_lamports,
+            &mut validation_buf,
+        )
     };
 
     let zd = Zendao {
@@ -126,16 +74,15 @@ fn it_should_call_entry_generated_by_anchors_macro() {
     buffer.seek(SeekFrom::Start(0)).unwrap();
     buffer.read_to_end(&mut buf).unwrap();
     let mut lamports = 1000;
-    let zendao_info = create_account_info(&info_key, &info_owner, &mut lamports, &mut buf);
-    
+    let zendao_info = factory::create_account_info(&info_key, &info_owner, &mut lamports, &mut buf);
 
     let mut buf: Vec<u8> = Vec::new();
     let mut lamports = 1000;
-    let founder_info = create_signer_account_info(&info_key, &info_owner, &mut lamports, &mut buf);
+    let founder_info = factory::create_signer_account_info(&info_key, &info_owner, &mut lamports, &mut buf);
 
     let mut buf: Vec<u8> = Vec::new();
     let mut lamports = 1000;
-    let program_info = create_program_account_info(&info_key, &info_owner, &mut lamports, &mut buf);
+    let program_info = factory::create_program_account_info(&info_key, &info_owner, &mut lamports, &mut buf);
     let accounts = &[zendao_info, validation_info, founder_info, program_info];
     entry(&program_id, accounts, &final_data).unwrap();
 }
@@ -146,22 +93,19 @@ fn it_just_runs() {
     let mut data: Vec<u8> = Vec::new();
     let mut lamports = 1000;
     let zendao = factory::create_zendao_account(&test_ctx, &mut lamports, &mut data);
-
     assert_eq!(zendao.min_balance, 1);
 
     let mut data: Vec<u8> = Vec::new();
     let mut lamports = 1000;
     let validation = factory::create_validation_account(&test_ctx, &mut lamports, &mut data);
 
-    let mut buf: Vec<u8> = Vec::new();
+    let mut data: Vec<u8> = Vec::new();
     let mut lamports = 1000;
-    let founder_info = create_signer_account_info(&test_ctx.info_key, &test_ctx.info_owner, &mut lamports, &mut buf);
-    let founder = Signer::try_from(&founder_info).unwrap();
+    let founder = factory::create_signer(&test_ctx, &mut lamports, &mut data);
 
-    let mut buf: Vec<u8> = Vec::new();
+    let mut data: Vec<u8> = Vec::new();
     let mut lamports = 1000;
-    let program_info = create_program_account_info(&test_ctx.info_key, &test_ctx.info_owner, &mut lamports, &mut buf);
-    let system_program = Program::try_from(&program_info).unwrap();
+    let system_program = factory::create_system_program(&test_ctx, &mut lamports, &mut data);
     let mut init_dao = InitDAO {
         zendao,
         validation,
@@ -180,7 +124,7 @@ fn it_just_runs() {
 
     let token = Pubkey::from_str("2QB8wEBJ8jjMQuZPvj3jaZP7JJb5j21u4xbxTnwsZRfv").unwrap();
     let min_balance = 10;
-    let dao_slug = String::from("slug");
+    let dao_slug = String::from("new_slug");
     let result = initialize(ctx, token, min_balance, dao_slug);
     // ctx foi retirado da memoria nesse escopo
     match result {
@@ -189,4 +133,6 @@ fn it_just_runs() {
     }
 
     assert_eq!(init_dao.validation.timestamp, 123);
+    assert_eq!(init_dao.zendao.slug, "new_slug");
+    assert_eq!(init_dao.zendao.min_balance, 10);
 }
